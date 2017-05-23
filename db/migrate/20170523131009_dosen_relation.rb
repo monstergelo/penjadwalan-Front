58686e4e654c8db010ce520054692624d6d9b97d
13514066 - Hendrikus Bimawan Satrianto <13514066@std.stei.itb.ac.id>
2017-05-23 21:34:13 +0700

    tambah association

---
+++ db/migrate/20170523131009_dosen_relation.rb
@@ -0,0 +1,4 @@
+class DosenRelation < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523111117) do
+ActiveRecord::Schema.define(version: 20170523122939) do
 
   create_table "calendar_events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "start_date"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 20170523111117) do
     t.string   "NIP"
     t.string   "nama"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.string   "name"
+    t.integer  "event_id"
+    t.integer  "event_type"
+    t.string   "owner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -49,6 +60,32 @@ ActiveRecord::Schema.define(version: 20170523111117) do
     t.integer  "dosen_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "periods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "start"
+    t.datetime "end"
+    t.integer  "period_type"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "scheduled_events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.datetime "end"
+    t.datetime "start"
+    t.string   "student_id"
+    t.string   "room_id"
+    t.integer  "event_type"
+    t.string   "event_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
